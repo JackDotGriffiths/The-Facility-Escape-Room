e32ed5b87c3c1a202626930ff13d09977efe1107
JackDotGriff <jack.griffiths@students.plymouth.ac.uk>
2018-12-07 10:44:02 +0000

    New Lightmaps + Main Menu

---
+++ Saw Themed Escape Room/Assets/Scripts/PuzzleRoom1/GoToMainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GoToMainMenu : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Invoke("LoadMainMenu", 1f);
+        }
+    }
+    
+    private void LoadMainMenu()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene(0);
+    }
+}

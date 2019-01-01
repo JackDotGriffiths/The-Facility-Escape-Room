5863c9e6012474930f7051860f3502281797a623
JackDotGriff <jack.griffiths@students.plymouth.ac.uk>
2019-01-01 23:50:19 +0000

    KeypadUI Created + Vent Maze Complete

---
+++ The Facility Escape Room/Assets/Scripts/PuzzleRoom2/KeypadButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class KeypadButton : MonoBehaviour {
+
+    public Text KeypadInput;
+    public int Value;
+
+    public CharacterController characterController;
+
+    // Use this for initialization
+    void Start () {
+        characterController = GameObject.Find("Player").GetComponent<CharacterController>();
+
+        characterController.enabled = false;
+        characterController.GetComponent<FirstPersonController>().enabled = false;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+
+        Value = int.Parse(this.gameObject.name);
+	}
+	
+	// Update is called once per frame
+	public void ButtonClick() {
+        Debug.Log("Click No. " + this.gameObject.name);
+        if (KeypadInput.text == "_ _ _ _")
+        {
+            KeypadInput.text = "";
+        }
+        if (KeypadInput.text.Length < 4)
+        {
+            string OldText = KeypadInput.text;
+            string NewText = KeypadInput.text + Value.ToString();
+            KeypadInput.text = NewText;
+        }
+        else
+        {
+            return;
+        }
+	}
+}

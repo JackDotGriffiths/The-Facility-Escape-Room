5ba36fb9477c6f44640561d8c24014fa1d1404d4
JackDotGriff <jack.griffiths@students.plymouth.ac.uk>
2019-01-10 21:40:47 +0000

    Escape/Entrance Sequence complete

---
+++ The Facility Escape Room/Assets/Scripts/EscapeEntranceControl.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class EscapeEntranceControl : MonoBehaviour {
+
+    public GameObject Player;
+    public CharacterController characterController;
+
+    public GameObject DoorToClose;
+
+    public GameObject Light1;
+    public GameObject Light2;
+    public GameObject Light3;
+    public GameObject Light4;
+    public GameObject Light5;
+
+    public Material LightOn;
+    public Material LightOff;
+
+    public AudioSource Entity;
+    public AudioClip Breathing;
+    public AudioClip Punch;
+
+    public GameObject NewSpawnPoint;
+
+    public string Message1;
+    public string Message2;
+
+    private bool PlayedSequence = false;
+
+    public void Start()
+    {
+        DoorToClose.GetComponent<Animator>().Play("DoorOpen");
+        DoorToClose.GetComponent<AudioSource>().Play();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && PlayedSequence == false)
+        {
+            PlayedSequence = true;
+            StartCoroutine(RunSequence());
+        }
+    }
+
+    IEnumerator RunSequence()
+    {
+        //Close Door Behind  the Player
+        DoorToClose.GetComponent<Animator>().Play("DoorClose");
+        DoorToClose.GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(1f);
+        
+        //Run Welcome message 1
+        ToolTipType.CreateTooltip(Message1);
+        yield return new WaitForSeconds(6f);
+
+        //Turn off Lights
+        TurnOffLight(Light1);
+        yield return new WaitForSeconds(3f);
+        TurnOffLight(Light2);
+        yield return new WaitForSeconds(2f);
+        TurnOffLight(Light3);
+        yield return new WaitForSeconds(2f);
+        TurnOffLight(Light4);
+        yield return new WaitForSeconds(4f);
+        TurnOffLight(Light5);
+        yield return new WaitForSeconds(2f);
+
+        //Slowly hear breathing.
+        Entity.volume = 0;
+        Entity.clip = Breathing;
+        Entity.loop = true;
+        Entity.Play();
+        for(int i=0; i < 100; i++)
+        {
+            Entity.volume += 0.005f;
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        //Punch the Player
+        yield return new WaitForSeconds(3f);
+
+        LockPlayerControls();
+        Entity.volume = 0.8f;
+        Entity.clip = Punch;
+        Entity.loop = false;
+        Entity.Play();
+        yield return new WaitForSeconds(0.1f);
+        Entity.volume = 0;
+
+        //Screen Go Black.
+        FadeScreenControl.Toggle();
+        Player.GetComponentInChildren<AudioListener>().enabled = false;
+        yield return new WaitForSeconds(2);
+        ToolTipType.CreateTooltip("Welcome to The Facility.");
+
+        yield return new WaitForSeconds(6);
+        Player.transform.position = NewSpawnPoint.transform.position;
+        Player.GetComponentInChildren<AudioListener>().enabled = true;
+        FadeScreenControl.Toggle();
+
+        TurnOnLight(Light1);
+        TurnOnLight(Light2);
+        TurnOnLight(Light3);
+        TurnOnLight(Light4);
+        TurnOnLight(Light5);
+
+
+
+
+
+    }
+
+    void TurnOffLight(GameObject light)
+    {
+        light.GetComponent<AudioSource>().Play();
+        light.GetComponent<MeshRenderer>().material = LightOff;
+        light.GetComponentInChildren<Light>().enabled = false;
+    }
+    void TurnOnLight(GameObject light)
+    {
+        light.GetComponent<MeshRenderer>().material = LightOn;
+        light.GetComponent<Light>().enabled = true;
+    }
+    void LockPlayerControls()
+    {
+        characterController.enabled = false;
+        characterController.GetComponent<FirstPersonController>().enabled = false;
+    }
+    void UnlockPlayerControls()
+    {
+        characterController.enabled = true;
+        characterController.GetComponent<FirstPersonController>().enabled = true;
+    }
+}
---
+++ The Facility Escape Room/Assets/Scripts/FadeScreenControl.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScreenControl : MonoBehaviour {
+
+    public Image BlackScreen;
+    public static bool ToggleBlack;
+
+    private bool CurrentlyBlack = false;
+
+    public static void Toggle()
+    {
+        ToggleBlack = true;
+    }
+
+    private void Update()
+    {
+        if (ToggleBlack != false)
+        {
+            Debug.Log("Toggling");
+            if (CurrentlyBlack == true)
+            {
+                Debug.Log("Going Clear");
+                ToggleBlack = false;
+                CurrentlyBlack = true;
+                StartCoroutine(GoToClear());
+            }
+            else
+            {
+                Debug.Log("Going Black");
+                CurrentlyBlack = true;
+                ToggleBlack = false;
+                StartCoroutine(GoToBlack());
+            }
+        }
+    }
+
+    IEnumerator GoToClear()
+    {
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            // set color with i as alpha
+            BlackScreen.color = new Color(0, 0, 0, i);
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+    }
+    IEnumerator GoToBlack()
+    {
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+            // set color with i as alpha
+            BlackScreen.color = new Color(0, 0, 0, i);
+            yield return null;
+        }
+
+    }
+
+
+}
